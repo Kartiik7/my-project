9c4ecd5789f7efb6aab702c0e6398c506860ec67
kartik2006p <k95241906@gmail.com>
2025-08-06 17:53:10 +0530

    Final Commit

--- Experiment-3/script.js
+++
@@ -1,7 +0,0 @@
-const toggleBtn = document.getElementById('theme-switcher');
-const body = document.body;
-
-toggleBtn.addEventListener('click', () => {
-  body.classList.toggle('dark-theme');
-  body.classList.toggle('light-theme');
-});
